1198454c5950789f39cb7cf0abe13b40fbfab972
Samir899 <sameerkmr823@gmail.com>
2025-12-27 20:10:27 +0530

    [Samir] Initial Commit

---
+++ src/test/java/com/proton_tech/oauth/OauthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.proton_tech.oauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OauthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
